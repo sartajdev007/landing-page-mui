4f756e35e611302850e63493f1eec3ada629816c
sartajdev007 <sartaj5akik@gmail.com>
2022-12-07 12:43:32 +0600

    branding section add

---
+++ src/components/Home/Branding.js
@@ -0,0 +1,42 @@
+import { Box, Button, Stack, styled, Typography } from '@mui/material';
+import React from 'react';
+
+const BrandingWrapper = styled(Box)(({ theme }) => ({
+    height: '70vh',
+    backgroundColor: 'rgba(122, 178, 89, 0.15)',
+    padding: theme.spacing(5),
+    margin: '4rem 0',
+    clipPath: 'polygon(0 0, 100% 0, 100% 85%, 0 100%)',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+}))
+
+const Branding = () => {
+    return <BrandingWrapper>
+        <Stack spacing={2}>
+            <Typography
+                sx={{
+                    fontSize: '2.5rem',
+                    fontWeight: 'bold',
+                    lineHeight: '1.2'
+                }}
+            >
+                Let’s Grow Your Brand <br /> To The Next Level
+            </Typography>
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, facilis!
+            </Typography>
+            <Button sx={{ width: '50%' }}>
+                Hire Us
+            </Button>
+        </Stack>
+        <Box
+            sx={{ width: "50%" }}
+        >
+            <img src="https://i.ibb.co/sPXVMbC/Frame.png" alt="" style={{ width: '100%' }} />
+        </Box>
+    </BrandingWrapper>
+};
+
+export default Branding;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Branding from './Branding';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Branding></Branding>
         </div>
     );
 };
